e54e4da882d1db729f7c5add2cb45450e58210c2
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2021-12-30 12:47:21 +0100

    ajout fichier modifiés avec le meme nom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState, useRef } from "react";
+//Import Styles
+import "./styles/app.scss";
+//Adding components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+
+//import de la data (musiques et infos musiques)
+import data from "./data";
 
 function App() {
+  //Ref : Permet d'attribuer une reférence à une balise HTML
+  const audioRef = useRef(null);
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  //State updaté avec les infos de la musique grâce à la fonction au dessus TimeUpdateHandler
+  const [songInfo, setSongInfo] = useState({
+    currenTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+  });
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  //Création d'une fonction qui récupère le temps et la durée de la musique
+  //une fois qu'on a récupérer les deux valeurs ont les passes dans setSonginfo pour créer un State updaté.
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    //Calculate pourcentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration: duration,
+      animationPercentage: animation,
+    });
+  };
+
+  const activeLibraryHandler = (nextPrev) => {
+    const newSongs = songs.map((song) => {
+      if (song.id === nextPrev.id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+  };
+
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex((songs) => songs.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    activeLibraryHandler(songs[(currentIndex + 1) % songs.length]);
+    if (isPlaying) audioRef.current.play();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      {/*Ici on passe le state de notre currentsong dans notre component song.
+      Les données sont envoyées vers le fils song depuis App.js qui va récupérer les infos dans DATA*/}
+      <Song currentSong={currentSong} />
+      <Player
+        audioRef={audioRef}
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        setSongs={setSongs}
+        activeLibraryHandler={activeLibraryHandler}
+      />
+      <Library
+        audioRef={audioRef}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        libraryStatus={libraryStatus}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        /*onLoadedMetadata permet de mettre à jour la duration directement au refresh et pas au moment ou on appuis sur play*/
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+        onEnded={songEndHandler}
+      ></audio>
     </div>
   );
 }
